f1cd86b0348c5363aac1e73b5334bcb6fa4ea6ef
teddylegesse <teddylegesse334@gmail.com>
2022-09-20 17:58:08 +0000

    lastdigit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+i#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        lastdigit = number % 10
+    elif number < 0:
+        lastdigit = (number * -1) % 10
+
+    print(lastdigit, end='')
+    return lastdigit
