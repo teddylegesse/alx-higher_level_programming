eb153ba345fa8650316e26ff04d9592236aba28d
teddylegesse <teddylegesse334@gmail.com>
2022-10-17 07:23:26 +0000

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,24 +3,20 @@
 module Square class
 """
 
-
 class Square:
-    """attributes of  a square."""
+    """attributes of a square."""
 
-    def __init__(self, size):
-        """Initialize a new Square.
+    def __init__(self, size=0):
+        """Initialize a new square.
         Args:
             size (int): The size of the new square.
         """
-	if not isinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        """Area of the square.
-        Returns:
-            thee size squared.
-        """
-        return self.__size ** 2
+        """area of the square."""
+        return (self.__size * self.__size)
