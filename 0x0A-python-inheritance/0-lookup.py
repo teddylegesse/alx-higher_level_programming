1a0e42db3da1e961c24e7e25d853a244ed582529
teddylegesse <teddylegesse334@gmail.com>
2022-10-24 16:43:54 +0000

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+definition of the function lookup
+"""
+
+
+def lookup(obj):
+    """Returns a list object
+    Args:
+        obj: object whose attributes and methods are to be returned
+    """
+
+    return (dir(obj))
