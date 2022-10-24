1942fa1666ac1fd87cd0e1bcd9324b1e34ae36f4
teddylegesse <teddylegesse334@gmail.com>
2022-10-24 17:00:03 +0000

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Contains definiton for the class MyList that inherits from list.
+"""
+class MyList(list):
+    """definiton for the class MyList that inherits from list.
+    """
+    def print_sorted(self):
+        """print sorted list"""
+        print(sorted(self))
