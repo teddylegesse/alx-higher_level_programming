0efb056d9068dd8b2d6c6c0cc0d11867d2a7c4a7
teddylegesse <teddylegesse334@gmail.com>
2022-10-25 18:36:00 +0000

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
+"""Module 7-add_item.
+Adds all arguments to a Python list,
+and then save them to a file.
+"""
+
 import sys
 import json
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import os.path
 
-args = sys.argv
-filename = "add_item.json"
-with open(filename, 'a+', encoding="utf-8") as f:
-    my_list = []
-    my_list.append(args[1:])
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+my_file = 'add_item.json'
+
+my_list = []
+
+if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
+    my_list = load_from_json_file(my_file)
+
+if len(sys.argv) > 1:
+    for elem in sys.argv[1:]:
+        my_list.append(elem)
+
+save_to_json_file(my_list, my_file)
